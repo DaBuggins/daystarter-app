83087e57f62b23f69e59f040d53cefb04619c3a1
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-09-13 11:56:11 +0100

    Update RandPics.jsx
    Removed fox

--- src/RandPics.jsx
+++ src/RandPics.jsx
@@ -1,20 +1,18 @@
-import Fox from "./Fox";
 import Dog from "./Dog";
 import Cat from "./Cat";
 import PetsOutlinedIcon from "@mui/icons-material/PetsOutlined";
 
 export default function RandPics() {
-  const pics = [<Fox />, <Dog />, <Cat />];
+  const pics = [<Cat />, <Dog />];
 
   return (
     <div className="randpics">
       <PetsOutlinedIcon />
       {pics.map((pic) => (
-        <h2 className="memeimg" key={Math.random()}>
+        <div className="memeimg" key={Math.random()}>
           {pic}
-        </h2>
+        </div>
       ))}
     </div>
   );
 }
-
