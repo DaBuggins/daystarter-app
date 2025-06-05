b6fb03053e9d8c9b196cecf5a104ddc19cddb951
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2025-06-05 12:10:46 +0100

    Update Daystarter.jsx

--- src/Daystarter.jsx
+++ src/Daystarter.jsx
@@ -1,12 +1,12 @@
 import Advice from "./Advice";
-import GameQuote from "./GameQuote";
-import RandPics from "./RandPics";
-import Grid from "@mui/material/Grid2";
-import Clock2 from "./Clock2";
-import Comic2 from "./Comic2";
-import Currency from "./Currency";
-import Title from "./Title";
-import BodyColor from "./BodyColor";
+// import GameQuote from "./GameQuote";
+// import RandPics from "./RandPics";
+// import Grid from "@mui/material/Grid2";
+// import Clock2 from "./Clock2";
+// import Comic2 from "./Comic2";
+// import Currency from "./Currency";
+// import Title from "./Title";
+// import BodyColor from "./BodyColor";
 import Footer from "./Footers/Footer";
 
 export default function Daystarter() {
