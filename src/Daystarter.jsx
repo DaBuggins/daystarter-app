7ebee72e585c6d5a882d247712e2a50ccc67977d
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2025-06-05 12:10:17 +0100

    Update Daystarter.jsx

--- src/Daystarter.jsx
+++ src/Daystarter.jsx
@@ -12,19 +12,19 @@ import Footer from "./Footers/Footer";
 export default function Daystarter() {
   return (
     <>
-      <Grid container spacing={2} className="mainbody">
+{/*       <Grid container spacing={2} className="mainbody">
         <Grid xs={12} sm={6} lg={4}>
           <Title />
           <Advice />
-{/*           <Comic2 />
+          <Comic2 />
           <GameQuote />
         </Grid>
         <Grid xs={12} sm={6} lg={4}>
           <Clock2 />
           <RandPics />
-          <Currency /> */}
+          <Currency />
         </Grid>
-      </Grid>
+      </Grid> */}
       <Footer />
     </>
   );
